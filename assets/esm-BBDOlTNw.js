d4eee1a096516c723161185d3514f5b502fac9bc
Adityaraj3136 <Adityaraj3136@users.noreply.github.com>
2026-07-25 07:30:11 +0000

    deploy: e370ad1c645cb8b40be71fc37dd09ce0c2f0d7ea

---
+++ assets/esm-BBDOlTNw.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DNN_uLMS.js","assets/index-Ccknv_q-.js","assets/index-3ozcOYFr.css"])))=>i.map(i=>d[i]);
+import{a as e,r as t}from"./index-Ccknv_q-.js";var n=e(`BackgroundTask`,{web:()=>t(()=>import(`./web-DNN_uLMS.js`).then(e=>new e.BackgroundTaskWeb),__vite__mapDeps([0,1,2]))});export{n as BackgroundTask};
--- assets/web-BX28ZX9w.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./index-BDLym3R9.js";var t=class extends e{beforeExit(e){throw this.unimplemented(`Not implemented on web.`)}finish(e){throw this.unimplemented(`Not implemented on web.`)}};export{t as BackgroundTaskWeb};
