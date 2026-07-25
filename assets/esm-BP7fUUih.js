a1e9aed139fa1a85a83fc6893d1b0ef89e60d498
Adityaraj3136 <Adityaraj3136@users.noreply.github.com>
2026-07-25 07:20:34 +0000

    deploy: 87c77a6fdecdcfc4d07fc03b158b6907eb7d2e7f

---
+++ assets/esm-BP7fUUih.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BX28ZX9w.js","assets/index-BDLym3R9.js","assets/index-3ozcOYFr.css"])))=>i.map(i=>d[i]);
+import{a as e,r as t}from"./index-BDLym3R9.js";var n=e(`BackgroundTask`,{web:()=>t(()=>import(`./web-BX28ZX9w.js`).then(e=>new e.BackgroundTaskWeb),__vite__mapDeps([0,1,2]))});export{n as BackgroundTask};
