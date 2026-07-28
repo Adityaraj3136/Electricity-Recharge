e907c3f6a52eddc9b89410e0e11fb62c3b0e21dc
Adityaraj3136 <Adityaraj3136@users.noreply.github.com>
2026-07-28 12:04:28 +0000

    deploy: 8d60bad7aa3414bd361658ab7d6e6c813e442912

--- assets/esm-YN97V2bK.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DUoXsQgp.js","assets/index-Dgf4AlrS.js","assets/index-CCYRpeqM.css"])))=>i.map(i=>d[i]);
-import{a as e,r as t}from"./index-Dgf4AlrS.js";var n=e(`BackgroundTask`,{web:()=>t(()=>import(`./web-DUoXsQgp.js`).then(e=>new e.BackgroundTaskWeb),__vite__mapDeps([0,1,2]))});export{n as BackgroundTask};
--- assets/web-DUoXsQgp.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./index-Dgf4AlrS.js";var t=class extends e{beforeExit(e){throw this.unimplemented(`Not implemented on web.`)}finish(e){throw this.unimplemented(`Not implemented on web.`)}};export{t as BackgroundTaskWeb};
