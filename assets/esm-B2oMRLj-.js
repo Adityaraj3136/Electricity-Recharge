d8f0f42158c65710e66cc46feed51d6d2eab1df6
sgpeworks <sgpeworks@users.noreply.github.com>
2026-08-02 21:01:24 +0000

    deploy: 5f251d92262c951a39d5cfc72f10594e1c6aa906

---
+++ assets/esm-B2oMRLj-.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CHlhstBy.js","assets/index-CbHYdntJ.js","assets/index-DJrqqdgR.css"])))=>i.map(i=>d[i]);
+import{n as e,r as t}from"./index-CbHYdntJ.js";var n=e(`BackgroundTask`,{web:()=>t(()=>import(`./web-CHlhstBy.js`).then(e=>new e.BackgroundTaskWeb),__vite__mapDeps([0,1,2]))});export{n as BackgroundTask};
