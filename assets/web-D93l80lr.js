bdd9729ffd4ad4bf80e603c48ae2715ce3a0da73
sgpeworks <sgpeworks@users.noreply.github.com>
2026-08-02 20:10:18 +0000

    deploy: 825efa6fc9e49e1568aef10b087fb803c1aae739

---
+++ assets/web-D93l80lr.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-BNo38UD6.js";var t=class extends e{beforeExit(e){throw this.unimplemented(`Not implemented on web.`)}finish(e){throw this.unimplemented(`Not implemented on web.`)}};export{t as BackgroundTaskWeb};
