e5fd134a03bd44d56a1094efcfcb6f2cc5e9c727
sgpeworks <sgpeworks@users.noreply.github.com>
2026-08-02 19:25:07 +0000

    deploy: 5e63c7ae7a5acc29b93fb13d5aa6ebd90ea2b018

---
+++ assets/esm-CS9-_CXV.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DSRsZ7Pk.js","assets/index-B7ArJcbb.js","assets/index-DCWIL153.css"])))=>i.map(i=>d[i]);
+import{n as e,r as t}from"./index-B7ArJcbb.js";var n=e(`BackgroundTask`,{web:()=>t(()=>import(`./web-DSRsZ7Pk.js`).then(e=>new e.BackgroundTaskWeb),__vite__mapDeps([0,1,2]))});export{n as BackgroundTask};
