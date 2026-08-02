98253030b59b90fcce3a5c4e38b4332582dbe80a
sgpeworks <sgpeworks@users.noreply.github.com>
2026-08-02 20:52:21 +0000

    deploy: 1c4ac330066afd744a63e7e3703746bec824921e

--- assets/esm-V5uEckPR.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-ClAnEvpm.js","assets/index-Bsnt-v8H.js","assets/index-DKQ_PZSr.css"])))=>i.map(i=>d[i]);
-import{n as e,r as t}from"./index-Bsnt-v8H.js";var n=e(`BackgroundTask`,{web:()=>t(()=>import(`./web-ClAnEvpm.js`).then(e=>new e.BackgroundTaskWeb),__vite__mapDeps([0,1,2]))});export{n as BackgroundTask};
---
+++ assets/web-D6ggqK-V.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-Cbd4pVV4.js";var t=class extends e{beforeExit(e){throw this.unimplemented(`Not implemented on web.`)}finish(e){throw this.unimplemented(`Not implemented on web.`)}};export{t as BackgroundTaskWeb};
