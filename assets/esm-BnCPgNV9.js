e506c8628e3b2d1cc549c83ff6b1e0762729c0b2
sgpeworks <sgpeworks@users.noreply.github.com>
2026-08-02 20:56:18 +0000

    deploy: 1eea16f55466fde66fd4afd232273807a19ee210

---
+++ assets/esm-BnCPgNV9.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Bt_PoYSn.js","assets/index-DpaQiPns.js","assets/index-B_jw61Hp.css"])))=>i.map(i=>d[i]);
+import{n as e,r as t}from"./index-DpaQiPns.js";var n=e(`BackgroundTask`,{web:()=>t(()=>import(`./web-Bt_PoYSn.js`).then(e=>new e.BackgroundTaskWeb),__vite__mapDeps([0,1,2]))});export{n as BackgroundTask};
--- assets/esm-Bwb-DwCl.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-D6ggqK-V.js","assets/index-Cbd4pVV4.js","assets/index-DgkJ9j6N.css"])))=>i.map(i=>d[i]);
-import{n as e,r as t}from"./index-Cbd4pVV4.js";var n=e(`BackgroundTask`,{web:()=>t(()=>import(`./web-D6ggqK-V.js`).then(e=>new e.BackgroundTaskWeb),__vite__mapDeps([0,1,2]))});export{n as BackgroundTask};
