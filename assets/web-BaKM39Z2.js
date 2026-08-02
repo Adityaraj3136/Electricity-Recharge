d2932c9c97bb524e30b3e1582ed30e54cfb9676b
sgpeworks <sgpeworks@users.noreply.github.com>
2026-08-02 20:42:56 +0000

    deploy: 9c620011f4a7fe41315d77ee3179a55bcade2bdc

---
+++ assets/web-BaKM39Z2.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-CBY30bDe.js";var t=class extends e{beforeExit(e){throw this.unimplemented(`Not implemented on web.`)}finish(e){throw this.unimplemented(`Not implemented on web.`)}};export{t as BackgroundTaskWeb};
